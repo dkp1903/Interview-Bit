66b52fae7937028b39bdfb1fa66c848b848a9708
Dushyant Pathak <dushyantpathak6789@gmail.com>
2019-05-19 21:30:08 +0530

    Added a solution that works

--- Arrays/SpiralOrderMatrix-II.cpp
+++ Arrays/SpiralOrderMatrix-II.cpp
@@ -1,5 +1,5 @@
 // https://www.interviewbit.com/problems/spiral-order-matrix-ii/
-
+/*
 vector<vector<int> > Solution::generateMatrix(int A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
@@ -56,5 +56,57 @@ vector<vector<int> > Solution::generateMatrix(int A) {
     }
     
     return sol;
+    
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+//This is a solution that works
+
+
+
+vector<vector<int> > Solution::generateMatrix(int A)
+    {
+    int t = 0,l = 0, r = A - 1 ,b = A - 1;
+    int d = 0 , count = 1;
+    vector<vector<int> >a(A,vector<int>(A));
+    while(t <= b && l <= r){
+        if(d == 0){
+            for(int i = l;i <= r;i++){
+                a[t][i]=count;
+                count++;
+            }
+            d=1;
+            t++;
+        }
+        else if(d==1){
+            for(int i=t;i<=b;i++){
+                a[i][r]=count;
+                count++;
+            }
+            d=2;
+            r--;
+        }
+        else if(d==2){
+            for(int i=r;i>=l;i--){
+                a[b][i]=count;
+                count++;
+            }
+            d=3;
+            b--;
+        }
+        else if(d==3){
+            for(int i=b;i>=t;i--){
+                a[i][l]=count;
+                count++;
+            }
+            d=0;
+            l++;
+        }
+    }
+    return a;
     
 }
