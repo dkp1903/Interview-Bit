a08ddf66a51fdfd7083127dfd9178d78d4a782a9
Dushyant Pathak <dushyantpathak6789@gmail.com>
2019-05-19 10:43:12 +0530

    Explanation updated

--- Arrays/Noble Integer.cpp
+++ Arrays/Noble Integer.cpp
@@ -11,9 +11,14 @@ int Solution::solve(vector<int> &A) {
         while(i+1 < n && A[i] == A[i+1]){
             i++;
         }
-        //Since the array is sorted, 
+        //Since the array is sorted, A[i+1]>= A[i] always
+        //We break out of the inner while loop when A[i]!=A[i+1], that is, we see a different element upcoming.
+        //hence, we need to check if A[i] is noble, which is done using the if statement below.
         
-        if(n - (i+1) == A[i]){
+        if(n - (i+1) == A[i])//i +1 refers to the number of elements we have traversed so far, including the element we are on.
+            //hence n-(i+1) refers to the number of elements after the one we are checking.
+            //So if that count is equal to A[i], test for nobility is satisfied
+        {
             ans = 1;
             break;
         }
